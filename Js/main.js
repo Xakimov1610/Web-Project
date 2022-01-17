7d22579e2ba454d643e519fd0e5d261aaeafec1c
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-01-17 10:17:01 +0500

    Banner

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,13 @@
 // Header Scroll
 
-let nav = document
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
 
 // nav hide
 
