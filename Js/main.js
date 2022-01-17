fb843e96e81499e65258188301cd74e6ca55c02e
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-01-17 23:33:24 +0500

    PART 1

--- Js/main.js
+++ Js/main.js
@@ -10,6 +10,34 @@ window.onscroll = function(){
 }
 
 // nav hide
-
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".nav-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
 
 // Owl Carousel
+$(document).ready(function(){
+    $(".client-slider-section").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+});
